7bf0106cddcb3a757d21fdf07975b05d607dca70
sai6855 <fayazceg@gmail.com>
2026-03-04 20:19:45 +0530

    fix

--- packages/mui-utils/src/elementAcceptingRef/elementAcceptingRef.ts
+++ packages/mui-utils/src/elementAcceptingRef/elementAcceptingRef.ts
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import PropTypes from 'prop-types';
 import chainPropTypes from '../chainPropTypes';
 
@@ -8,58 +9,68 @@ function isClassComponent(elementType: Function) {
   return Boolean(prototype.isReactComponent);
 }
 
-function acceptingRef(
-  props: { [key: string]: unknown },
-  propName: string,
-  componentName: string,
-  location: string,
-  propFullName: string,
-) {
-  const element = props[propName];
-  const safePropName = propFullName || propName;
-
-  if (
-    element == null ||
-    // When server-side rendering React doesn't warn either.
-    // This is not an accurate check for SSR.
-    // This is only in place for Emotion compat.
-    // TODO: Revisit once https://github.com/facebook/react/issues/20047 is resolved.
-    typeof window === 'undefined'
+ export function createValidator(mode: 'element' | 'elementType') {
+  return function acceptingRef(
+    props: { [key: string]: unknown },
+    propName: string,
+    componentName: string,
+    location: string,
+    propFullName: string,
   ) {
+    const propValue = props[propName];
+    const safePropName = propFullName || propName;
+
+    if (
+      propValue == null ||
+      // When server-side rendering React doesn't warn either.
+      // This is only in place for Emotion compat.
+      // TODO: Revisit once https://github.com/facebook/react/issues/20047 is resolved.
+      typeof window === 'undefined'
+    ) {
+      return null;
+    }
+
+    let warningHint;
+
+    const valueToCheck = mode === 'element' ? (propValue as any).type : propValue;
+
+    /**
+     * Blacklisting instead of whitelisting
+     *
+     * Blacklisting will miss some components, such as React.Fragment. Those will at least
+     * trigger a warning in React.
+     * We can't whitelist because there is no safe way to detect React.forwardRef
+     * or class components. "Safe" means there's no public API.
+     */
+    if (typeof valueToCheck === 'function' && !isClassComponent(valueToCheck)) {
+      warningHint =
+        mode === 'element'
+          ? 'Did you accidentally use a plain function component for an element instead?'
+          : 'Did you accidentally provide a plain function component instead?';
+    }
+
+    if (mode === 'elementType' && propValue === React.Fragment) {
+      warningHint = 'Did you accidentally provide a React.Fragment instead?';
+    }
+
+    if (warningHint !== undefined) {
+      const noun = mode === 'element' ? 'element' : 'element type';
+      return new Error(
+        `Invalid ${location} \`${safePropName}\` supplied to \`${componentName}\`. ` +
+          `Expected an ${noun} that can hold a ref. ${warningHint} ` +
+          'For more information see https://mui.com/r/caveat-with-refs-guide',
+      );
+    }
+
     return null;
-  }
-
-  let warningHint;
-
-  const elementType: unknown = (element as any).type;
-  /**
-   * Blacklisting instead of whitelisting
-   *
-   * Blacklisting will miss some components, such as React.Fragment. Those will at least
-   * trigger a warning in React.
-   * We can't whitelist because there is no safe way to detect React.forwardRef
-   * or class components. "Safe" means there's no public API.
-   *
-   */
-  if (typeof elementType === 'function' && !isClassComponent(elementType)) {
-    warningHint = 'Did you accidentally use a plain function component for an element instead?';
-  }
-
-  if (warningHint !== undefined) {
-    return new Error(
-      `Invalid ${location} \`${safePropName}\` supplied to \`${componentName}\`. ` +
-        `Expected an element that can hold a ref. ${warningHint} ` +
-        'For more information see https://mui.com/r/caveat-with-refs-guide',
-    );
-  }
-
-  return null;
+  };
 }
+const elementValidator = createValidator('element');
 
 const elementAcceptingRef = chainPropTypes(
   PropTypes.element,
-  acceptingRef,
+  elementValidator,
 ) as PropTypes.Requireable<unknown>;
-elementAcceptingRef.isRequired = chainPropTypes(PropTypes.element.isRequired, acceptingRef);
+elementAcceptingRef.isRequired = chainPropTypes(PropTypes.element.isRequired, elementValidator);
 
 export default elementAcceptingRef;
